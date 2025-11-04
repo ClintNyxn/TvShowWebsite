06fd8bdf28990b7956b3bbbad3a5cb716b697c59
ClintNyxn <nyxnclint@gmail.com>
2025-11-04 13:03:06 +0530

    api fix. play and bkmark btn added

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const API_KEY = "f43bc26577a5235f5bc1f8ba8f17f742";
 const box = document.querySelector("main");
+const template = document.querySelector("#template-card");
 
 fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
-  .then(r => r.json())
+  .then(res => res.json())
   .then(d => {
     d.results.forEach(m => {
-      box.innerHTML += `
-        <div class="card">
-          <img src="https://image.tmdb.org/t/p/w500${m.poster_path}" alt="${m.title}">
-          <h4>${m.title}</h4>
-          <p>${m.release_date}</p>
-        </div>`;
+      const clone = template.cloneNode(true);
+      clone.style.display = "flex";
+      clone.querySelector("img").src = `https://image.tmdb.org/t/p/w500${m.poster_path}`;
+      clone.querySelector("h4").textContent = m.title;
+      clone.querySelector("p").textContent = m.release_date;
+      box.appendChild(clone);
     });
   })
-  .catch(err => console.error("Error fetching movies:", err));
+  .catch(console.error);
+;
